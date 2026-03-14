b252593de08b3dc9237ee8fb6b60cd8c405c75a6
Harold Bautista <harold200217@hotmail.com>
2026-03-13 20:07:20 -0500

    Menu arrays

--- arraylist/Main.java
+++ arraylist/Main.java
@@ -9,63 +9,33 @@ import arraylist.models.types.Terrestre;
 
 public class Main {
 
-    
-
     public static void main(String[] args) {
-        // Crear los arrays de transportes
-        ArrayList<Terrestre> transportes_terrestres = new ArrayList<Terrestre>();
-        ArrayList<Maritimo> transportes_maritimos   = new ArrayList<Maritimo>();
-        ArrayList<Aereo>    transportes_aereos      = new ArrayList<Aereo>();
 
-        // // Llenar los arrays de transportes
+        ArrayList<Terrestre> transportes_terrestres = new ArrayList<Terrestre>();
+        ArrayList<Maritimo> transportes_maritimos = new ArrayList<Maritimo>();
+        ArrayList<Aereo> transportes_aereos = new ArrayList<Aereo>();
+
         llenarTransportesTerrestres(transportes_terrestres);
         llenarTransportesMaritimos(transportes_maritimos);
         llenarTransportesAereos(transportes_aereos);
 
-        // Imprimir los arrays de transportes
+        Menu.mostrarMenu(transportes_terrestres, transportes_maritimos, transportes_aereos);
+
         printTransportesTerrestres(transportes_terrestres);
         printTransportesMaritimos(transportes_maritimos);
         printTransportesAereos(transportes_aereos);
 
-        // Añadir un nuevo transporte terrestre en la posición 2
-        añadirNuevoTransporte(2, transportes_terrestres, new Terrestre("Bicicleta", "Terrestre", "carretera", 2, 2));
-        printTransportesTerrestres(transportes_terrestres);
+        ArrayList<Transporte> transportes = new ArrayList<Transporte>();
+        transportes.addAll(transportes_terrestres);
+        transportes.addAll(transportes_maritimos);
+        transportes.addAll(transportes_aereos);
+        System.out.println("\nResumen De Transportes\n");
 
-
-        // Obtener el tamaño del ArrayList de transportes terrestres
-        obtenerTamañoDelArrayList(transportes_terrestres);
-
-        // Obtener el objeto por posición
-        obtenerObjetoPorPosicion(2, transportes_terrestres);
-
-        // Obtener el objeto por nombre
-        final Aereo transporte_uno = obtenerObjetoPorNombre("Aereo", transportes_aereos);
-        if(transporte_uno != null) 
-            System.out.println("Transporte terrestre: " + transporte_uno.getNombre());
-
-
-        // // Modificar el objeto por posición
-        ModificarObjetoPorPosicion(2, transportes_aereos, new Aereo("Drone dos . 0", "Aereo", 3000, 30, 3));
-        printTransportesAereos(transportes_aereos);
-
-        // // Eliminar el objeto por posición
-        eliminarObjetoPorPosicion(2, transportes_aereos);
-        printTransportesAereos(transportes_aereos);
-
-
-
-        // ArrayList<Transporte> transportes = new ArrayList<Transporte>();
-        // transportes.addAll(transportes_terrestres);
-        // transportes.addAll(transportes_maritimos);
-        // transportes.addAll(transportes_aereos);
-        
-
-        // for(Transporte transporte : transportes) {
-        //     System.out.println("Transporte: " + transporte.getNombre() + " - " + transporte.getMedio());
-        // }
+        for (Transporte transporte : transportes) {
+            System.out.println("Transporte: " + transporte.getNombre() + " - " + transporte.getMedio());
+        }
 
     }
-
 
     public static void llenarTransportesTerrestres(ArrayList<Terrestre> transportes_terrestres) {
         transportes_terrestres.add(new Terrestre("Tren", "Terrestre", "rieles", 30000, 0));
@@ -73,12 +43,12 @@ public class Main {
         transportes_terrestres.add(new Terrestre("automovil", "Terrestre", "carretera", 5, 4));
         transportes_terrestres.add(new Terrestre("motocicleta", "Terrestre", "carretera", 2, 2));
     }
-    
+
     public static void llenarTransportesMaritimos(ArrayList<Maritimo> transportes_maritimos) {
         transportes_maritimos.add(new Maritimo("Barco", "Maritimo", 10000, 100, 10));
         transportes_maritimos.add(new Maritimo("Yate", "Maritimo", 5000, 50, 5));
         transportes_maritimos.add(new Maritimo("Lancha", "Maritimo", 3000, 30, 3));
-        transportes_maritimos.add(new Maritimo("Lancha", "Maritimo", 3000, 30, 3));
+
     }
 
     public static void llenarTransportesAereos(ArrayList<Aereo> transportes_aereos) {
@@ -89,31 +59,30 @@ public class Main {
 
     public static void printTransportesTerrestres(ArrayList<Terrestre> transportes_terrestres) {
         System.out.println("*****************TERRESTRES********************");
-        System.out.println("==============================================");
+        System.out.println("===============================================");
 
-        for(Terrestre terrestre : transportes_terrestres) {
+        for (Terrestre terrestre : transportes_terrestres) {
             System.out.println("Transportes Terrestres: " + terrestre.getNombre() + " - " + terrestre.getMedio());
         }
     }
-        
+
     public static void printTransportesMaritimos(ArrayList<Maritimo> transportes_maritimos) {
         System.out.println("*****************MARITIMOS********************");
         System.out.println("==============================================");
 
-        for(Maritimo maritimo : transportes_maritimos) {
+        for (Maritimo maritimo : transportes_maritimos) {
             System.out.println("Transportes Maritimos: " + maritimo.getNombre() + " - " + maritimo.getMedio());
         }
     }
 
     public static void printTransportesAereos(ArrayList<Aereo> transportes_aereos) {
-        System.out.println("*****************AEREOS********************");
+        System.out.println("********************AEREOS********************");
         System.out.println("==============================================");
 
-        for(Aereo aereo : transportes_aereos) {
+        for (Aereo aereo : transportes_aereos) {
             System.out.println("Transportes Aereos: " + aereo.getNombre() + " - " + aereo.getMedio());
         }
     }
-
 
     public static <T> void añadirNuevoTransporte(int posicion, ArrayList<T> transportes_type, T transporte) {
         System.out.println("Añadiendo un nuevo transporte en la posición " + posicion);
@@ -125,15 +94,15 @@ public class Main {
     }
 
     public static <T extends Transporte> void obtenerObjetoPorPosicion(int posicion, ArrayList<T> transportes_type) {
-        System.out.println("Objeto en la posición " + posicion + " la instancia del objeto en memoria: " + transportes_type.get(posicion));
+        System.out.println("Objeto en la posición " + posicion + " la instancia del objeto en memoria: "
+                + transportes_type.get(posicion));
         System.out.println("Objeto en la posición " + posicion + ": " + transportes_type.get(posicion).getCapacidad());
     }
 
-
     public static <T extends Transporte> T obtenerObjetoPorNombre(String nombre, ArrayList<T> transportes_type) {
         try {
-            for(T transporte : transportes_type) {
-                if(transporte.getNombre().equals(nombre)) {
+            for (T transporte : transportes_type) {
+                if (transporte.getNombre().equals(nombre)) {
                     return transporte;
                 }
             }
@@ -143,8 +112,9 @@ public class Main {
             return null;
         }
     }
-    
-    public static <T extends Transporte> void ModificarObjetoPorPosicion(int posicion, ArrayList<T> transportes_type, T transporte) {
+
+    public static <T extends Transporte> void ModificarObjetoPorPosicion(int posicion, ArrayList<T> transportes_type,
+            T transporte) {
         System.out.println("Modificando el objeto en la posición " + posicion);
         transportes_type.set(posicion, transporte);
     }
@@ -154,4 +124,3 @@ public class Main {
         transportes_type.remove(posicion);
     }
 }
-
---
+++ arraylist/Menu.java
@@ -0,0 +1,179 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import arraylist.models.types.Aereo;
+import arraylist.models.types.Maritimo;
+import arraylist.models.types.Terrestre;
+
+public class Menu {
+
+    public static void mostrarMenu(
+            ArrayList<Terrestre> terrestres,
+            ArrayList<Maritimo> maritimos,
+            ArrayList<Aereo> aereos) {
+
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        int tipo;
+
+        do {
+
+            System.out.println("\n========= MENU =========");
+            System.out.println("1. Crear transporte");
+            System.out.println("2. Buscar transporte");
+            System.out.println("3. Actualizar transporte");
+            System.out.println("4. Eliminar transporte");
+            System.out.println("5. Mostrar transportes");
+            System.out.println("6. Salir");
+            System.out.print("Seleccione una opcion: ");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            if (opcion >= 1 && opcion <= 4) {
+
+                System.out.println("\nTipo de transporte:");
+                System.out.println("1. Terrestre");
+                System.out.println("2. Maritimo");
+                System.out.println("3. Aereo");
+                System.out.print("Seleccione tipo: ");
+
+                tipo = sc.nextInt();
+                sc.nextLine();
+
+            } else {
+                tipo = 0;
+            }
+
+            switch (opcion) {
+
+                // CREAR
+                case 1:
+
+                    System.out.print("Nombre: ");
+                    String nombre = sc.nextLine();
+
+                    if (tipo == 1) {
+
+                        terrestres.add(
+                                new Terrestre(nombre, "Terrestre", "carretera", 4, 4));
+
+                    } else if (tipo == 2) {
+
+                        maritimos.add(
+                                new Maritimo(nombre, "Maritimo", 100, 10, 2));
+
+                    } else if (tipo == 3) {
+
+                        aereos.add(
+                                new Aereo(nombre, "Aereo", 500, 20, 2));
+
+                    }
+
+                    System.out.println("Transporte creado");
+                    break;
+
+                // BUSCAR
+                case 2:
+
+                    System.out.print("Nombre a buscar: ");
+                    String buscar = sc.nextLine();
+
+                    if (tipo == 1) {
+
+                        Terrestre t = Main.obtenerObjetoPorNombre(buscar, terrestres);
+                        if (t != null)
+                            System.out.println("Encontrado: " + t.getNombre());
+
+                    } else if (tipo == 2) {
+
+                        Maritimo m = Main.obtenerObjetoPorNombre(buscar, maritimos);
+                        if (m != null)
+                            System.out.println("Encontrado: " + m.getNombre());
+
+                    } else if (tipo == 3) {
+
+                        Aereo a = Main.obtenerObjetoPorNombre(buscar, aereos);
+                        if (a != null)
+                            System.out.println("Encontrado: " + a.getNombre());
+
+                    }
+
+                    break;
+
+                // ACTUALIZAR
+                case 3:
+
+                    System.out.print("Posicion a actualizar: ");
+                    int pos = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Nuevo nombre: ");
+                    String nuevo = sc.nextLine();
+
+                    if (tipo == 1) {
+
+                        Main.ModificarObjetoPorPosicion(
+                                pos,
+                                terrestres,
+                                new Terrestre(nuevo, "Terrestre", "carretera", 4, 4));
+
+                    } else if (tipo == 2) {
+
+                        Main.ModificarObjetoPorPosicion(
+                                pos,
+                                maritimos,
+                                new Maritimo(nuevo, "Maritimo", 100, 10, 2));
+
+                    } else if (tipo == 3) {
+
+                        Main.ModificarObjetoPorPosicion(
+                                pos,
+                                aereos,
+                                new Aereo(nuevo, "Aereo", 500, 20, 2));
+
+                    }
+
+                    break;
+
+                // ELIMINAR
+                case 4:
+
+                    System.out.print("Posicion a eliminar: ");
+                    int eliminar = sc.nextInt();
+
+                    if (tipo == 1)
+                        Main.eliminarObjetoPorPosicion(eliminar, terrestres);
+
+                    else if (tipo == 2)
+                        Main.eliminarObjetoPorPosicion(eliminar, maritimos);
+
+                    else if (tipo == 3)
+                        Main.eliminarObjetoPorPosicion(eliminar, aereos);
+
+                    break;
+
+                // MOSTRAR
+                case 5:
+
+                    Main.printTransportesTerrestres(terrestres);
+                    Main.printTransportesMaritimos(maritimos);
+                    Main.printTransportesAereos(aereos);
+
+                    break;
+
+                case 6:
+                    System.out.println("Saliendo del programa...");
+                    break;
+
+                default:
+                    System.out.println("Opcion invalida");
+
+            }
+
+        } while (opcion != 6);
+        sc.close();
+    }
+}
