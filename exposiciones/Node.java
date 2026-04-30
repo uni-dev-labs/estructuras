d7ee0046defa5f5b4e41eea8683f6f4dcca90f8d
sebas28rc <sr401311@gmail.com>
2026-04-30 08:16:30 -0500

    este es el ejercicio 2

---
+++ exposiciones/Node.java
@@ -0,0 +1,28 @@
+package exposiciones;
+
+public class Node {
+
+    int value;
+    Node left;
+    Node right;
+
+    Node(int value) {
+        this.value = value;
+    }
+
+    void insert(int newValue) {
+        if (newValue < value) {
+            if (left == null) {
+                left = new Node(newValue);
+            } else {
+                left.insert(newValue);
+            }
+        } else {
+            if (right == null) {
+                right = new Node(newValue);
+            } else {
+                right.insert(newValue);
+            }
+        }
+    }
+}
